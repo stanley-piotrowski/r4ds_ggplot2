ba1502801b383e964b1e2b78d8d34e0391647c4f
Stanley Piotrowski <stanleyjpiotrowski@gmail.com>
2022-01-25 09:59:36 -0800

    Moved outside of scripts

---
+++ plot_theme.R
@@ -0,0 +1,9 @@
+# plot_theme.R ------------------------------------------------------------
+
+# Set ggplot2 theme
+plot_theme <- ggplot2::theme(
+  panel.background = ggplot2::element_rect(color = "black", fill = "white"),
+  panel.grid = ggplot2::element_blank()
+)
+
+
